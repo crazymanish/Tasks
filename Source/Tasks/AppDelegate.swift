9db1390a2d29fc2a08e7827b27d3297d94812db0
Manish Rathi <m.rathi@catawiki.nl>
2020-04-06 23:00:07 +0200

    Initial commit - Created an empty macOS application.

---
+++ Source/Tasks/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tasks
+//
+//  Created by Manish on 06/04/2020.
+//  Copyright © 2020 Manish Rathi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Source/Tasks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Manish on 06/04/2020.
+//  Copyright © 2020 Manish Rathi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
